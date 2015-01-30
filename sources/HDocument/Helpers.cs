83faa0b0d2d95544e8f2cf9db78e19a47a8e9296
Yanos <ygrenier@ygrenier.com>
2015-01-30 07:40:23 +0100

    Lookat node helpers.

--- sources/HDocument/Helpers.cs
+++ sources/HDocument/Helpers.cs
@@ -13,22 +13,34 @@ namespace HDoc
     {
 
         /// <summary>
-        /// Insert the content after a node
+        /// Insert content after a node
         /// </summary>
         /// <param name="parent">Parent</param>
         /// <param name="node">Node</param>
         /// <param name="content">Content to insert.</param>
         public static void InsertAfter(HContainer parent, HNode node, params object[] content)
         {
-            if (content == null || content.Length == 0) return;
+            if (parent == null || content == null || content.Length == 0) return;
             HNode anchor = node;
             foreach (var cnt in content)
-                InsertContentAfter(parent, ref anchor, cnt);
+                anchor = InsertContentAfter(parent, anchor, cnt);
         }
 
-        static void InsertContentAfter(HContainer parent, ref HNode node, object content)
+        static HNode InsertContentAfter(HContainer parent, HNode node, object content)
         {
+            // If content null stop insert
+            if (content == null) return node;
 
+            // Parent is empty ?
+            if (parent.content == null)
+            {
+                // 
+            }
+            else
+            {
+
+            }
+            return node;
         }
     }
 
