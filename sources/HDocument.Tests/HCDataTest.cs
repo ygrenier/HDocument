cd0b4350ee3f5eb1a18b66287e54d95265bd465d
Yan Grenier <ygrenier@ygrenier.com>
2015-01-31 20:57:12 +0100

    Adding CData

---
+++ sources/HDocument.Tests/HCDataTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HDoc.Tests
+{
+    public class HCDataTest
+    {
+
+        [Fact]
+        public void TestCreate()
+        {
+            HCData cdata = new HCData("Test");
+            Assert.Equal("Test", cdata.Value);
+        }
+
+        [Fact]
+        public void TestCreateFromOther()
+        {
+            HCData cdata = new HCData("Test");
+            Assert.Equal("Test", cdata.Value);
+
+            HCData ncdata = new HCData(cdata);
+            Assert.Equal("Test", ncdata.Value);
+
+        }
+
+        [Fact]
+        public void TestClone()
+        {
+            HCData cdata = new HCData("Test");
+
+            HContainer parent = new HElement("test");
+
+            parent.Add(cdata);
+            Assert.Same(cdata, parent.FirstNode);
+            Assert.Same(cdata, parent.LastNode);
+
+            HContainer otherParent = new HElement("test");
+            // Do clone
+            otherParent.Add(cdata);
+
+            Assert.IsType<HCData>(otherParent.FirstNode);
+            Assert.NotSame(otherParent.FirstNode, parent.FirstNode);
+
+
+
+        }
+
+    }
+}
---
+++ sources/HDocument/HCData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HDoc
+{
+
+    /// <summary>
+    /// CData node
+    /// </summary>
+    public class HCData : HText
+    {
+
+        /// <summary>
+        /// Create a new CData node
+        /// </summary>
+        public HCData(string value) : base(value) { }
+
+        /// <summary>
+        /// Create a new CData from another XCData object.
+        /// </summary>
+        public HCData(HCData other) : base(other) { }
+
+        /// <summary>
+        /// Clone the node
+        /// </summary>
+        internal override HNode CloneNode() {
+            return new HCData(this);
+        }
+
+    }
+
+}
