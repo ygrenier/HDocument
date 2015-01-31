fa31388b7a9385537c2a523535ac8c80702f0214
Yan Grenier <ygrenier@ygrenier.com>
2015-01-31 23:30:36 +0100

    Add the comment node. Tests.

---
+++ sources/HDocument.Tests/HCommentTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HDoc.Tests
+{
+    public class HCommentTest
+    {
+        [Fact]
+        public void TestCreate()
+        {
+            var hComment = new HComment("Content");
+            Assert.Equal("Content", hComment.Value);
+
+            hComment = new HComment("");
+            Assert.Equal("", hComment.Value);
+
+            Assert.Throws<ArgumentNullException>(() => new HComment((String)null));
+        }
+
+        [Fact]
+        public void TestCreateFromOther()
+        {
+            var hOtherComment = new HComment("Content");
+
+            var hComment = new HComment(hOtherComment);
+            Assert.Equal("Content", hComment.Value);
+
+            Assert.Throws<ArgumentNullException>(() => new HComment((HComment)null));
+        }
+
+        [Fact]
+        public void TestValue()
+        {
+            var hComment = new HComment("Content");
+            Assert.Equal("Content", hComment.Value);
+            hComment.Value = "Other content";
+            Assert.Equal("Other content", hComment.Value);
+
+            Assert.Throws<ArgumentNullException>(() => hComment.Value = null);
+        }
+
+        [Fact]
+        public void TestClone()
+        {
+            HComment hComment = new HComment("Test");
+
+            HContainer parent = new HElement("test");
+
+            parent.Add(hComment);
+            Assert.Same(hComment, parent.FirstNode);
+            Assert.Same(hComment, parent.LastNode);
+
+            HContainer otherParent = new HElement("test");
+            // Do clone
+            otherParent.Add(hComment);
+
+            Assert.IsType<HComment>(otherParent.FirstNode);
+            Assert.NotSame(otherParent.FirstNode, parent.FirstNode);
+        }
+
+    }
+}
---
+++ sources/HDocument/HComment.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HDoc
+{
+    /// <summary>
+    /// Html comment.
+    /// </summary>
+    public class HComment : HNode
+    {
+        internal string value;
+
+        /// <summary>
+        /// Create a new comment node.
+        /// </summary>
+        public HComment(String value)
+        {
+            if (value == null) throw new ArgumentNullException("value");
+            this.value = value;
+        }
+
+        /// <summary>
+        /// Create a new comment from another.
+        /// </summary>
+        public HComment(HComment other)
+        {
+            if (other == null) throw new ArgumentNullException("other");
+            this.value = other.value;
+        }
+
+        internal override HNode CloneNode()
+        {
+            return new HComment(this);
+        }
+
+        /// <summary>
+        /// Get or set the content of this comment.
+        /// </summary>
+        public String Value
+        {
+            get { return this.value; }
+            set
+            {
+                if (value == null) throw new ArgumentNullException("value");
+                this.value = value;
+            }
+        }
+    }
+
+}
