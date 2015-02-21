ec98b13f8f22fb38f4539b843f759ac09fb5c0c2
Yan Grenier <ygrenier@ygrenier.com>
2015-02-21 16:09:09 +0100

    Implements HasClass() method.

---
+++ sources/HDocument.Tests/HQuery/ClassAttributeTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HDoc.Tests.HQuery
+{
+    public class ClassAttributeTest
+    {
+        [Fact]
+        public void TestGetClasses()
+        {
+            Assert.Equal(new String[] { }, ClassAttributeExtensions.GetClasses(null));
+
+            var element = new HElement("div");
+            Assert.Equal(new String[] { }, element.GetClasses());
+
+            var clsAttr = new HAttribute("class");
+            element.Add(clsAttr);
+
+            Assert.Equal(new String[] { }, element.GetClasses());
+
+            clsAttr.Value = "    ";
+            Assert.Equal(new String[] { }, element.GetClasses());
+
+            clsAttr.Value = "  class1  ";
+            Assert.Equal(new String[] { "class1" }, element.GetClasses());
+
+            clsAttr.Value = "  class1 class-2 ";
+            Assert.Equal(new String[] { "class1", "class-2" }, element.GetClasses());
+
+        }
+
+        [Fact]
+        public void TestHasClass()
+        {
+            var elements = new HElement[]{
+                new HElement("p", "First paragraph."),
+                null,
+                new HElement("p", new HAttribute("class", "selected"), "Second paragraph is selected.")
+            };
+            Assert.False(elements[0].HasClass(null));
+            Assert.False(elements[0].HasClass("selected"));
+            Assert.False(elements[1].HasClass("selected"));
+            Assert.True(elements[2].HasClass("selected"));
+            Assert.False(elements.HasClass(null));
+            Assert.False(elements.HasClass("not-selected"));
+            Assert.True(elements.HasClass("selected"));
+        }
+
+    }
+}
---
+++ sources/HDocument/HQuery/ClassAttributeExtensions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HDoc
+{
+
+    /// <summary>
+    /// Class Attribute manipulation methods
+    /// </summary>
+    public static class ClassAttributeExtensions
+    {
+
+        /// <summary>
+        /// Extract classes from an element
+        /// </summary>
+        public static String[] GetClasses(this HElement element)
+        {
+            HAttribute clsAttr = element != null ? element.Attribute("class") : null;
+            if (clsAttr == null|| clsAttr.Value==null) return new String[0];
+            return clsAttr.Value.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        /// <summary>
+        /// Determine if the element is assigned the given class.
+        /// </summary>
+        /// <param name="element">Source element.</param>
+        /// <param name="className">The class name to search for.</param>
+        /// <returns>Returns true if the class is assigned.</returns>
+        public static bool HasClass(this HElement element, String className)
+        {
+            // If invalid argument then not found
+            if (element == null || string.IsNullOrWhiteSpace(className))
+                return false;
+            var cls = element.GetClasses();
+            return cls.Any(c => String.Equals(className, c, StringComparison.OrdinalIgnoreCase));
+        }
+
+        /// <summary>
+        /// Determine wether any of the matched elements are assigned the given class.
+        /// </summary>
+        /// <param name="elements">Source elements.</param>
+        /// <param name="className">The class name to search for.</param>
+        /// <returns>Returns true if the class is assigned to an element.</returns>
+        public static bool HasClass(this IEnumerable<HElement> elements, String className)
+        {
+            // If invalid argument then not found
+            if (elements == null || string.IsNullOrWhiteSpace(className))
+                return false;
+            // Browse elements
+            foreach (var element in elements)
+            {
+                if (element == null) continue;
+                if (element.HasClass(className))
+                    return true;
+            }
+            return false;
+        }
+
+        // TODO AddClass
+        // TODO RemoveClass
+        // TODO ToggleClass
+    }
+
+}
