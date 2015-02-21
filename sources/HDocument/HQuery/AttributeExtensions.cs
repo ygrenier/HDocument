66f3ae5c53498a1df9f8ae971cd0fda15b0a6777
Yan Grenier <ygrenier@ygrenier.com>
2015-02-21 20:17:38 +0100

    Implements the basics methods.

--- sources/HDocument/HQuery/AttributeExtensions.cs
+++ sources/HDocument/HQuery/AttributeExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace HDoc
@@ -38,6 +39,104 @@ namespace HDoc
                 }
             }
             return element;
+        }
+
+        /// <summary>
+        /// Get the value of the attribute of the element.
+        /// </summary>
+        public static String Attr(this HElement element, String name)
+        {
+            if (element != null && !String.IsNullOrWhiteSpace(name))
+            {
+                var attr = element.Attribute(name);
+                if (attr != null)
+                    return attr.Value;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Get the value of an attribute for the first element in the set of matched elements.
+        /// </summary>
+        public static String Attr(this IEnumerable<HElement> elements, String name)
+        {
+            if (elements != null && !String.IsNullOrWhiteSpace(name))
+            {
+                return elements.FirstOrDefault(e => e != null).Attr(name);
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Set one attribute for the element.
+        /// </summary>
+        public static HElement Attr(this HElement element, String name, String value)
+        {
+            return element.Attr(name, value);
+        }
+
+        /// <summary>
+        /// Set one attribute for the set of matched elements.
+        /// </summary>
+        public static IEnumerable<HElement> Attr(this IEnumerable<HElement> elements, String name, String value)
+        {
+            if (elements != null && !String.IsNullOrWhiteSpace(name))
+            {
+                foreach (var element in elements)
+                {
+                    elements.Attr(name, value);
+                }
+            }
+            return elements;
+        }
+
+        /// <summary>
+        /// Set one or more attributes from a POCO objet for the element.
+        /// </summary>
+        public static HElement Attr(this HElement element, Object attributes)
+        {
+            if (element != null && attributes != null)
+            {
+                foreach (var member in attributes.GetType().GetMembers().Where(m => m is PropertyInfo || m is FieldInfo))
+                {
+                    if (member is PropertyInfo)
+                        element.Attr(member.Name, Convert.ToString(((PropertyInfo)member).GetValue(attributes, null)));
+                    else if (member is FieldInfo)
+                        element.Attr(member.Name, Convert.ToString(((FieldInfo)member).GetValue(attributes)));
+                }
+            }
+            return element;
+        }
+
+        /// <summary>
+        /// Set one or more attributes from a POCO objet for the set of matched elements.
+        /// </summary>
+        public static IEnumerable<HElement> Attr(this IEnumerable<HElement> elements, Object attributes)
+        {
+            if (elements != null && attributes != null)
+            {
+                foreach (var element in elements)
+                {
+                    elements.Attr(attributes);
+                }
+            }
+            return elements;
+        }
+
+        /// <summary>
+        /// Set one attribute for the set of matched elements with a valeu from a callback method.
+        /// </summary>
+        public static IEnumerable<HElement> Attr(this IEnumerable<HElement> elements, String name, Func<HElement, int, String> getValue)
+        {
+            if (elements != null && !String.IsNullOrWhiteSpace(name) && getValue != null)
+            {
+                int idx = 0;
+                foreach (var element in elements)
+                {
+                    element.Attr(name, getValue(element, idx++));
+                }
+            }
+            return elements;
         }
     }
 }
