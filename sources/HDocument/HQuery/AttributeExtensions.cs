d584557e2481e0210dfcb98404d827644dfba44d
Yan Grenier <ygrenier@ygrenier.com>
2015-02-24 08:52:51 +0100

    First read Css() methods implementation.

--- sources/HDocument/HQuery/AttributeExtensions.cs
+++ sources/HDocument/HQuery/AttributeExtensions.cs
@@ -11,6 +11,9 @@ namespace HDoc
     /// </summary>
     public static class AttributeExtensions
     {
+
+        #region Attribute/Attr
+
         /// <summary>
         /// Set one attribute for the element.
         /// </summary>
@@ -139,5 +142,122 @@ namespace HDoc
             }
             return elements;
         }
+
+        #endregion
+
+        #region CSS
+
+        /// <summary>
+        /// Convert a styleName to style-name.
+        /// </summary>
+        static String ConvertCamelNameToStyleName(String name)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (var c in name)
+            {
+                if (Char.IsUpper(c))
+                {
+                    if (result.Length > 0) result.Append('-');
+                    result.Append(Char.ToLower(c));
+                }
+                else
+                    result.Append(c);
+            }
+            return result.ToString();
+        }
+
+        /// <summary>
+        /// Extract all style properties of the element
+        /// </summary>
+        public static IDictionary<String, String> Css(this HElement element)
+        {
+            String style = element.Attr("style");
+            var result = new Dictionary<String, String>(StringComparer.OrdinalIgnoreCase);
+            if (!String.IsNullOrWhiteSpace(style))
+            {
+                // Split style
+                String[] parts = style.Split(new Char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var part in parts)
+                {
+                    String name, value;
+                    int p = part.IndexOf('=');
+                    if (p < 0)
+                    {
+                        name = part;
+                        value = String.Empty;
+                    }
+                    else
+                    {
+                        name = part.Substring(0, p);
+                        value = part.Substring(p + 1);
+                    }
+                    result[ConvertCamelNameToStyleName(name)] = value;
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Extract all style properties in the first element in the list
+        /// </summary>
+        public static IDictionary<String, String> Css(this IEnumerable<HElement> elements)
+        {
+            return elements.First().Css();
+        }
+
+        /// <summary>
+        /// Get the value of a style property in the element
+        /// </summary>
+        public static String Css(this HElement element, String propertyName)
+        {
+            var styles = element.Css();
+            String result;
+            if (styles.TryGetValue(ConvertCamelNameToStyleName(propertyName), out result))
+                return result;
+            return String.Empty;
+        }
+
+        /// <summary>
+        /// Get the value of a style property in the first element in the list
+        /// </summary>
+        public static String Css(this IEnumerable<HElement> elements, String propertyName)
+        {
+            return elements.First().Css(propertyName);
+        }
+
+        /// <summary>
+        /// Get the values of a list of style properties in the element
+        /// </summary>
+        public static IEnumerable<String> Css(this HElement element, params String[] propertyNames)
+        {
+            var styles = element.Css();
+            if (propertyNames != null)
+            {
+                foreach (var propertyName in propertyNames)
+                {
+                    if (String.IsNullOrWhiteSpace(propertyName)) continue;
+                    String result;
+                    if (styles.TryGetValue(ConvertCamelNameToStyleName(propertyName), out result))
+                        yield return result;
+                    else
+                        yield return String.Empty;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get the values of a list of style properties in the first element in the list
+        /// </summary>
+        public static IEnumerable<String> Css(this IEnumerable<HElement> elements, params String[] propertyNames)
+        {
+            return elements.First().Css(propertyNames);
+        }
+
+        // TODO css( String propertyName, String value )
+        // TODO css( String propertyName, Func<HElement, int, String> callback )
+        // TODO css( Object properties )
+
+        #endregion
+
     }
 }
