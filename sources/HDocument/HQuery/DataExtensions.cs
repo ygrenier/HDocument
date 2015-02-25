ff5632788970b4e0cfba8de2c57db0d963798749
Yan Grenier <ygrenier@ygrenier.com>
2015-02-25 21:03:28 +0100

    Create the methods. Without testings.

--- sources/HDocument/HQuery/DataExtensions.cs
+++ sources/HDocument/HQuery/DataExtensions.cs
@@ -10,13 +10,20 @@ namespace HDoc
     /// </summary>
     public static class DataExtensions
     {
+        /// <summary>
+        /// Build the data key from a key
+        /// </summary>
+        internal static String MakeDataKey(String key)
+        {
+            return String.Concat("data-", key);
+        }
 
         /// <summary>
         /// Retrieve a data value from an element
         /// </summary>
         public static String Data(this HElement element, String key)
         {
-            throw new NotImplementedException();
+            return element.Attr(MakeDataKey(key));
         }
 
         /// <summary>
@@ -24,7 +31,7 @@ namespace HDoc
         /// </summary>
         public static String Data(this IEnumerable<HElement> elements, String key)
         {
-            throw new NotImplementedException();
+            return (elements != null ? elements.FirstOrDefault() : null).Data(key);
         }
 
         /// <summary>
@@ -32,7 +39,7 @@ namespace HDoc
         /// </summary>
         public static HElement Data(this HElement element, String key, String value)
         {
-            throw new NotImplementedException();
+            return element.Data(MakeDataKey(key), value);
         }
 
         /// <summary>
@@ -40,7 +47,14 @@ namespace HDoc
         /// </summary>
         public static IEnumerable<HElement> Data(this IEnumerable<HElement> elements, String key, String value)
         {
-            throw new NotImplementedException();
+            if (elements != null)
+            {
+                foreach (var element in elements)
+                {
+                    element.Data(key, value);
+                }
+            }
+            return elements;
         }
 
         /// <summary>
@@ -48,7 +62,14 @@ namespace HDoc
         /// </summary>
         public static HElement Data(this HElement element, object values)
         {
-            throw new NotImplementedException();
+            if (element != null && values != null)
+            {
+                foreach (var kv in UtilHelpers.ExtractKeyValues(values))
+                {
+                    element.Data(MakeDataKey(kv.Key), kv.Value);
+                }
+            }
+            return element;
         }
 
         /// <summary>
@@ -56,23 +77,52 @@ namespace HDoc
         /// </summary>
         public static IEnumerable<HElement> Data(this IEnumerable<HElement> elements, object values)
         {
-            throw new NotImplementedException();
+            if (elements != null && values != null)
+            {
+                foreach (var element in elements)
+                {
+                    element.Data(values);
+                }
+            }
+            return elements;
         }
 
         /// <summary>
         /// Remove a list of data values to an element
         /// </summary>
-        public static HElement RemoveData(this HElement element, params String[] names)
+        public static HElement RemoveData(this HElement element, params String[] keys)
         {
-            throw new NotImplementedException();
+            // Build the key list from the context
+            if (keys != null && keys.Length > 0)
+            {
+                // Convert all keys to data keys
+                keys = keys.Select(k => MakeDataKey(k)).ToArray();
+            }
+            else
+            {
+                // Extract all data keys
+                keys = element.Attributes().Where(attr => attr.Name.StartsWith("data-", StringComparison.OrdinalIgnoreCase)).Select(attr => attr.Name).ToArray();
+            }
+            foreach (var key in keys)
+            {
+                element.Attr(key, null);
+            }
+            return element;
         }
 
         /// <summary>
         /// Remove a list of data values to a set of elements
         /// </summary>
-        public static IEnumerable<HElement> RemoveData(this  IEnumerable<HElement> elements, params String[] names)
+        public static IEnumerable<HElement> RemoveData(this  IEnumerable<HElement> elements, params String[] keys)
         {
-            throw new NotImplementedException();
+            if (elements != null)
+            {
+                foreach (var element in elements)
+                {
+                    element.RemoveData(keys);
+                }
+            }
+            return elements;
         }
 
         /// <summary>
@@ -80,7 +130,7 @@ namespace HDoc
         /// </summary>
         public static bool HasData(this HElement element)
         {
-            throw new NotImplementedException();
+            return element.Attributes().Where(attr => attr.Name.StartsWith("data-", StringComparison.OrdinalIgnoreCase)).Any();
         }
 
         /// <summary>
@@ -88,7 +138,7 @@ namespace HDoc
         /// </summary>
         public static bool HasData(this IEnumerable<HElement> elements)
         {
-            throw new NotImplementedException();
+            return (elements != null ? elements.FirstOrDefault() : null).HasData();
         }
 
         /// <summary>
@@ -96,7 +146,8 @@ namespace HDoc
         /// </summary>
         public static bool HasData(this HElement element, String key)
         {
-            throw new NotImplementedException();
+            if (String.IsNullOrWhiteSpace(key)) return false;
+            return element.Attribute(MakeDataKey(key)) != null;
         }
 
         /// <summary>
@@ -104,7 +155,7 @@ namespace HDoc
         /// </summary>
         public static bool HasData(this IEnumerable<HElement> elements, String key)
         {
-            throw new NotImplementedException();
+            return (elements != null ? elements.FirstOrDefault() : null).HasData(key);
         }
 
     }
---
+++ sources/HDocument/UtilHelpers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace HDoc
+{
+    /// <summary>
+    /// Some helpers
+    /// </summary>
+    public static class UtilHelpers
+    {
+        /// <summary>
+        /// Extract the key-value list from an objet.
+        /// </summary>
+        public static IDictionary<String, String> ExtractKeyValues(Object source, Func<String, String> keyTransform = null)
+        {
+            Dictionary<String, String> result = new Dictionary<string, string>();
+            keyTransform = keyTransform ?? (n => n);
+            if (source is IDictionary<String, String>)
+            {
+                foreach (var prop in ((IDictionary<String, String>)source))
+                    result[keyTransform(prop.Key)] = prop.Value;
+            }
+            else if (source != null)
+            {
+                foreach (var member in source.GetType().GetMembers().Where(m => m is PropertyInfo || m is FieldInfo))
+                {
+                    String mName = keyTransform(member.Name);
+                    if (member is PropertyInfo)
+                        result[mName] = Convert.ToString(((PropertyInfo)member).GetValue(source, null));
+                    else if (member is FieldInfo)
+                        result[mName] = Convert.ToString(((FieldInfo)member).GetValue(source));
+                }
+            }
+            return result;
+        }
+    }
+}
