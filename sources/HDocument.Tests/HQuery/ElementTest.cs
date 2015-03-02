5af8bc5d0236c763e8a04b8fa524b62c96a418f7
Yan Grenier <ygrenier@ygrenier.com>
2015-03-02 21:12:47 +0100

    After() implementation. Tests.

---
+++ sources/HDocument.Tests/HQuery/ElementTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HDoc.Tests.HQuery
+{
+    public class ElementTest
+    {
+        [Fact]
+        public void TestAfterElement()
+        {
+            var element1 = new HElement("h1");
+            var element2 = new HElement("h2");
+            var container = new HElement("div", element1, element2).AddClass("container");
+
+            element1.After(new HElement("p"));
+
+            Assert.Equal(new String[] { "h1", "p", "h2" }, container.Elements().Select(e => e.Name));
+
+            var ex = Assert.Throws<InvalidOperationException>(() => container.After(new HElement("p")));
+            Assert.Equal("No parent found.", ex.Message);
+        }
+
+        [Fact]
+        public void TestAfterElements()
+        {
+            var element1 = new HElement("h1");
+            var element2 = new HElement("h2");
+            var container = new HElement("div", element1, element2).AddClass("container");
+
+            container.Elements().After(new HElement("p"));
+
+            Assert.Equal(new String[] { "h1", "p", "h2", "p" }, container.Elements().Select(e => e.Name));
+
+        }
+
+        [Fact]
+        public void TestAfterElementsByCallback()
+        {
+            var element1 = new HElement("h1");
+            var element2 = new HElement("h2");
+            var container = new HElement("div", element1, element2).AddClass("container");
+
+            container.Elements().After((e, i) => new HElement[] { new HElement(e.Name + "-" + i.ToString()) });
+
+            Assert.Equal(new String[] { "h1", "h1-0", "h2", "h2-1" }, container.Elements().Select(e => e.Name));
+
+        }
+
+    }
+}
---
+++ sources/HDocument/HQuery/ElementExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HDoc
+{
+    /// <summary>
+    /// Element extensions
+    /// </summary>
+    public static class ElementExtensions
+    {
+
+        #region After()
+
+        /// <summary>
+        /// Add content after the element
+        /// </summary>
+        public static HElement After(this HElement element, params HElement[] content)
+        {
+            if (element != null && content != null)
+            {
+                element.AddAfter(content.Where(c => c != null).Cast<object>().ToArray());
+            }
+            return element;
+        }
+
+        /// <summary>
+        /// Add content after each element of a set of elements
+        /// </summary>
+        public static IEnumerable<HElement> After(this IEnumerable<HElement> elements, params HElement[] content)
+        {
+            if (elements != null && content != null)
+            {
+                foreach (var element in elements.ToList())
+                {
+                    element.After(content);
+                }
+            }
+            return elements;
+        }
+
+        /// <summary>
+        /// Add content from a callback after each element of a set of elements
+        /// </summary>
+        public static IEnumerable<HElement> After(this IEnumerable<HElement> elements, Func<HElement, int, IEnumerable<HElement>> getContent)
+        {
+            if (elements != null && getContent != null)
+            {
+                int idx = 0;
+                foreach (var element in elements.ToList())
+                {
+                    element.After(getContent(element, idx++).ToArray());
+                }
+            }
+            return elements;
+        }
+
+        #endregion
+
+        #region Before()
+
+
+
+        #endregion
+
+    }
+}
